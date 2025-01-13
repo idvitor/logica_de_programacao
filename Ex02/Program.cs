82ace5c5dc701cb1c4bc28f16d9eb0d523287fb0
JOAO VITOR CORREIA LEITE <joao.vcleite4@senacsp.edu.br>
2025-01-13 19:57:49 -0300

    first commit

---
+++ Ex02/Program.cs
@@ -0,0 +1,7 @@
+﻿// Ex.02 - Escreva um programa que pergunte o nome do usuário, armazene esse nome numa variável name e apresente uma mensagem de boas-vindas exibindo o nome do usuário
+
+Console.WriteLine("informe o seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("seja bem-vindo(a) " + name);
+
---
+++ Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ex03 - Escrever programa que solicite dois numeros inteiros para o usuário e exiba a soma dos números
+
+Console.WriteLine("digite o primeiro número: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("digite o segundo número: ");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("digite o terceiro número: ");
+int numero3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("digite o quarto número: ");
+int numero4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("a soma é: " + (numero1 + numero2 + numero3 +numero4));
+
+Console.WriteLine($"a soma é: + { numero1 + numero2 + numero3 + numero4} ");
+
+
